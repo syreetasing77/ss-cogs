1e546ab61483f6bbc31cf3f942b39da6c5b5a929
syreetasing77 <syreeta.singleton96@gmail.com>
2026-02-22 12:14:03 -0500

    Created spending tracker

---
+++ script.py
@@ -0,0 +1,20 @@
+# This program automatically tracks spending
+name = "Syreeta"  #this variable is a string
+mint = "altoids"
+battery = "duracell"
+weather = "snow"
+new_york = "fashion"
+southern = "hot"
+brooklynn = "brownsville"
+pay = 1200  #This variable is an integer
+wallet = 0
+degrees = 38
+time = 8
+version = 3.13  #This variable is a floating number
+fraction = 3.15
+author = "steven"
+class_today = 5
+
+pay_raise = 1.10
+increase = pay * pay_raise
+print("$",increase)
