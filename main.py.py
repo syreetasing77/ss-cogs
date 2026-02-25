755023f15ba3e03e8dc034631012638e2beca4f7
syreetasing77 <syreeta.singleton96@gmail.com>
2026-02-25 18:04:17 -0500

    added first_folder

---
+++ main.py.py
@@ -0,0 +1,8 @@
+# This program gives motivational words to help you get through whatever you need.
+name = "Syreeta"
+peace = "is necessary to think clearly"
+listening = "listen to when your body needs rest"
+keep_going = "never give up, keep moving"
+reset = "bubble baths, quiet time with a cup of tea"
+
+
